c38598484e94abc13756d51e375c40abeef139d0
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2024-01-24 18:59:41 +0330

    ✅ memoized state

--- src/components/tabs/Tab.tsx
+++ src/components/tabs/Tab.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import TabHead from "./TabHead";
 import useStore from "@/store/useStore";
 import TabBody from "./TabBody";
@@ -24,28 +24,45 @@ export default function Tab() {
 
   const { tasks, toggleTask, deleteTask, completedTaskLength, allTaskLength } = useStore();
 
-  const [tabs, setTabs] = useState<Tab[]>([]);
-
   const [allTask, setAllTask] = useState<Task[]>([]);
   const [completedTasks, setCompletedTasks] = useState<Task[]>([]);
   const [activeTasks, setActiveTasks] = useState<Task[]>([]);
+  const [tabs, setTabs] = useState<Tab[]>([]);
+
+  const memoToggleTask = useCallback(
+    (id: number) => {
+      toggleTask(id);
+    },
+    [toggleTask]
+  );
+
+  const memoDeleteTask = useCallback(
+    (id: number) => {
+      deleteTask(id);
+    },
+    [deleteTask]
+  );
+
+  useEffect(() => {
+    setAllTask(tasks);
+  }, [tasks]);
 
   useEffect(() => {
     const completedTasks = tasks.filter((task) => task.completed);
     const activeTasks = tasks.filter((task) => !task.completed);
-
-    setAllTask(tasks);
     setCompletedTasks(completedTasks);
     setActiveTasks(activeTasks);
-
-    setTabs([
-      { name: 'All', href: '?tab=all', count: tasks.length, current: "all" },
-      { name: 'Completed!', href: '?tab=completed', count: completedTasks.length, current: "completed" },
-      { name: 'Active', href: '?tab=active', count: activeTasks.length, current: "active" },
-    ]);
-
   }, [tasks]);
 
+  const tabsMemo = useMemo(() => [
+    { name: 'All', href: '?tab=all', count: allTask.length, current: "all" },
+    { name: 'Completed!', href: '?tab=completed', count: completedTasks.length, current: "completed" },
+    { name: 'Active', href: '?tab=active', count: activeTasks.length, current: "active" },
+  ], [allTask, completedTasks, activeTasks]);
+
+  useEffect(() => {
+    setTabs(tabsMemo);
+  }, [tabsMemo]);
 
   return (
     <>
